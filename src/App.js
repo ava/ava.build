fe92dc6ac9ce4fbb0e971035c5f739f792160861
Alex Cory <results@alexcory.com>
2019-08-03 20:01:26 -0700

    quick and dirty website

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Snowflakes, Center, Peeled, Quote, SocialMedia } from './components'
+
+
+const App = () => {
+  
+  return (
+    <Snowflakes>
+      <SocialMedia />
+      <Center>
+        <Logo />
+        <Quote>Rapid Application Development</Quote>
+        <Peeled phrase='Ava' color='rgb(137, 183, 44)' />
+      </Center>
+    </Snowflakes>
+  )
+}
+
+const Logo = styled.img.attrs({
+  src: `${window.location.origin}/ava-logo-green.png`
+})`
+  width: 262px;
+`
+
+export default App;
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { SocialLink } from '.'
+import { media } from '../utils'
+
+const Container = styled.div`
+  position: absolute;
+  color: white;
+  ${media.phone`
+    display: flex;
+    margin: 8px;
+  `}
+  width: 50px;
+  margin: 8px 0;
+  text-align: center;
+`
+
+export const SocialMedia = () => (
+  <Container>
+    <SocialLink type='github' url='https://github.com/avainc' />
+    <SocialLink type='linkedin' url='https://linkedin.com/in/avaassociates' />
+    {/* <SocialLink type='codepen' url='https://codepen.io/alex-cory/' /> */}
+    <SocialLink type='twitter' url='https://twitter.com/avaassociates' />
+    <SocialLink type='facebook' url='https://fb.com/avaassociates' />
+    {/* <SocialLink type='medium' url='https://medium.com/@alexcory' /> */}
+    {/* <SocialLink type='jsfiddle' url='https://jsfiddle.net/user/alexcory/fiddles/' /> */}
+    <SocialLink type='email' url='mailto:alex@ava.inc' />
+    {/* Runkit: https://runkit.com/alex-cory */}
+    {/* Codesandbox: https://codesandbox.io/u/alex-cory */}
+  </Container>
+)
